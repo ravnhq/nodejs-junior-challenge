357bcd66633e9fa79ef4adafe9980612e3ee5161
FrankFuentes21 <franciscofuentes@ravn.co>
2021-08-04 11:26:04 -0600

    test: add unit tests for calls cost challenge

--- src/challenges.spec.js
+++ src/challenges.spec.js
@@ -1,4 +1,6 @@
+const callsCost = require("./calls-cost")
 const fibonacci = require("./fibonacci")
+const { calls } = require("./mocks/challenges.mocks")
 const { fibonacciSerie } = require("./mocks/tests.mocks")
 
 describe('fibonacci challenge', () => {
@@ -16,5 +18,30 @@ describe('fibonacci challenge', () => {
     const fibonacciNumbers = fibonacci(30)
     expect(fibonacciNumbers).toHaveProperty('length', 30)
     expect(fibonacciNumbers).toEqual(fibonacciSerie.slice(0, 30))
+  })
+})
+
+describe('calls challenge', () => {
+  it('should process calls array', () => {
+    const callsOperation1 = callsCost(calls.slice(0, 5))
+    const callsOperation2 = callsCost(calls)
+
+    expect(callsOperation1).toHaveProperty('totalCalls', 5)
+    expect(callsOperation1).toHaveProperty('callsDetails')
+    expect(callsOperation1).toHaveProperty('total', 65.74)
+    callsOperation1.callsDetails.forEach(
+      call => {
+        expect(call).toHaveProperty('identifier', 'type', 'duration', 'callCost')
+      }
+    )
+
+    expect(callsOperation2).toHaveProperty('totalCalls', 6)
+    expect(callsOperation2).toHaveProperty('callsDetails')
+    expect(callsOperation2).toHaveProperty('total', 67.34)
+    callsOperation2.callsDetails.forEach(
+      call => {
+        expect(call).toHaveProperty('identifier', 'type', 'duration', 'callCost')
+      }
+    )
   })
 })
