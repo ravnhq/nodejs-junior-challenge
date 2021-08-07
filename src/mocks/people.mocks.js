440e3e6b19a3e7b1fcc0422168f82086d637c0d5
FrankFuentes21 <franciscofuentes@ravn.co>
2021-08-07 16:03:13 -0600

    feat: add challenge to process people information

---
+++ src/mocks/people.mocks.js
@@ -0,0 +1,81 @@
+const people = [
+  {
+    name: 'Julio',
+    lastname: 'Cesar',
+    age: 36,
+    weight: 163,
+    height: 190
+  },
+  {
+    name: 'Charlie',
+    lastname: 'Snox',
+    age: 84,
+    weight: 200,
+    height: 190
+  },
+  {
+    name: 'John',
+    lastname: 'Doe',
+    age: 15,
+    weight: 159,
+    height: 170
+  },
+  {
+    name: 'Petter',
+    lastname: 'Parker',
+    age: 29,
+    weight: 150,
+    height: 150
+  },
+  {
+    name: 'Gary',
+    lastname: 'Nodal',
+    age: 54,
+    weight: 165,
+    height: 175
+  },
+  {
+    name: 'Julius',
+    lastname: 'Martir',
+    age: 67,
+    weight: 159,
+    height: 180
+  },
+  {
+    name: 'Salma',
+    lastname: 'Smith',
+    age: 33,
+    weight: 170,
+    height: 171
+  },
+  {
+    name: 'Tatiana',
+    lastname: 'Soliban',
+    age: 59,
+    weight: 179,
+    height: 170
+  },
+  {
+    name: 'Michael',
+    lastname: 'Salvador',
+    age: 65,
+    weight: 183,
+    height: 191
+  },
+  {
+    name: 'Katia',
+    lastname: 'Moran',
+    age: 41,
+    weight: 120,
+    height: 160
+  },
+  {
+    name: 'Patty',
+    lastname: 'Lue',
+    age: 66,
+    weight: 133,
+    height: 187
+  }
+]
+
+module.exports = { people }
---
+++ src/people.js
@@ -0,0 +1,10 @@
+/*
+The height in cm of several people was taken, in addition, the age of each person and extra information are also known.
+An algorithm is required to calculate average age and average height. In addition, we need to know the information of the 
+tallest person, and the information of the youngest person.
+*/
+
+
+function peopleInformation(people) { }
+
+module.exports = peopleInformation
