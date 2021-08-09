a59c7a2fa875d20ed4927ed3225ccae78b6ce134
FrankFuentes21 <franciscofuentes@ravn.co>
2021-08-09 11:28:28 -0600

    docs: add methods documentation

--- src/morse-code.js
+++ src/morse-code.js
@@ -1,6 +1,11 @@
-/*
-Write a program that automatically converts english text to morse code and vice versa, consider only use upper case letters 
-when converting
+/** 
+ * Write a program that automatically converts english text to morse code and vice versa, consider only use upper case letters 
+ * when converting
+ * 
+ * @param {string} message - String to be converted
+ * @param {string} convertTo - (morse | english) 
+ * 
+ * @returns {string}  - String converted to english or morse code
 */
 
 function morseCode(message, convertTo) { }
