6245ed25ec6d3fbd6a67fb6ce87558d9149bb581
FrankFuentes21 <franciscofuentes@ravn.co>
2021-08-08 21:44:11 -0600

    feat: add morse code challenge

---
+++ src/morse-code.js
@@ -0,0 +1,8 @@
+/*
+Write a program that automatically converts english text to morse code and vice versa, consider only use upper case letters 
+when converting
+*/
+
+function morseCode(message, convertTo) { }
+
+module.exports = morseCode
