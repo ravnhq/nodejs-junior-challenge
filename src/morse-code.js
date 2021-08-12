2e9cdb0a7ea5a47fa7ee0cd9ce2ccf81567ec5a7
Patrick Lazo <patricklazo@ravn.co>
2021-08-12 18:34:44 -0500

    feat: add solution morseCode

--- src/morse-code.js
+++ src/morse-code.js
@@ -8,6 +8,43 @@
  * @returns {string}  - String converted to english or morse code
 */
 
-function morseCode(message, convertTo) { }
+function morseCode(message, convertTo) {
+    const englishToMorse = new Map([
+        ['A', '.-'], ['B', '-...'], ['C', '-.-.'], ['D', '-..'],
+        ['E', '.'], ['F', '..-.'], ['G', '--.'], ['H', '....'],
+        ['I', '..'], ['J', '.---'], ['K', '-.-'], ['L', '.-..'],
+        ['M', '--'], ['N', '-.'], ['O', '---'], ['P', '.--.'],
+        ['Q', '--.-'], ['R', '.-.'], ['S', '...'], ['T', '-'],
+        ['U', '..-'], ['V', '...-'], ['W', '.--'], ['X', '-..-'],
+        ['Y', '-.--'], ['Z', '--..'], ['0', '-----'], ['1', '.----'],
+        ['2', '..---'], ['3', '...--'], ['4', '....-'], ['5', '.....'],
+        ['6', '-....'], ['7', '--...'], ['8', '---..'], ['9', '----.'],
+        [':', '---...'], [',', '--..--'], ['-', '-....-'], ['(', '-.--.-'],
+        ['.', '.-.-.-'], ['?', '..--..'], [';', '-.-.-.'], ['/', '-..-.'],
+        ['_', '..--.-'], [')', '---..'], ['=', '-...-'], ['@', '.--.-.'],
+        ['+', '.-.-.'], [' ', ' ']
+    ]);
+    const morseToEnglish = new Map([...englishToMorse.entries()].map(([key, value]) => ([value, key])));
+    let output = [];
+    let tmp;
+
+    if (convertTo === 'morse') {
+        for (char of message) {
+            output.push(englishToMorse.get(char.toUpperCase()));
+        }
+    }
+
+    if (convertTo === 'english') {
+        for (word of message.split('   ')) {
+            tmp = '';
+            for (code of word.split(' ')) {
+                tmp += morseToEnglish.get(code);
+            }
+            output.push(tmp);
+        }
+    }
+
+    return output.join(' ');
+}
 
 module.exports = morseCode
