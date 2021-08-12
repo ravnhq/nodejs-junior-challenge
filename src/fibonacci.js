8ee14e00a17b2303dd6e24cd4f5d26306910a3e6
Patrick Lazo <patricklazo@ravn.co>
2021-08-12 14:58:14 -0500

    feat: add solution fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,11 +1,19 @@
-/** 
+/**
  * Given a number, write a function that returns the "n" numbers of the fibonacci sequence
- *  
+ *
  * @param {number} limit - Number of fibonacci numbers to be generated
- * 
+ *
  * @returns {number[]}  - Fibonacci Serie
-*/
+ */
 
-function fibonacci(limit) { }
+function fibonacci(limit) {
+  const fibonacci = [0, 1];
+
+  for (let i = 2; i < limit; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+  }
+
+  return fibonacci;
+}
 
 module.exports = fibonacci
