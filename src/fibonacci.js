5d3b098af48b2d284c41dc63578141674a1d6a64
Kevin Cotrina <kcotrinam92@gmail.com>
2021-08-11 20:51:22 -0500

    Add fibonacci method implementation

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,11 +1,27 @@
-/** 
+/**
  * Given a number, write a function that returns the "n" numbers of the fibonacci sequence
- *  
+ *
  * @param {number} limit - Number of fibonacci numbers to be generated
- * 
+ *
  * @returns {number[]}  - Fibonacci Serie
-*/
+ */
 
-function fibonacci(limit) { }
+function fibonacci(limit) {
+  let prevNum = 0,
+    currentNum = 1,
+    fibonacci = [];
 
-module.exports = fibonacci
+  while (limit > 0) {
+    fibonacci.push(prevNum);
+    let temp = prevNum;
+    prevNum = currentNum;
+    currentNum = temp + prevNum;
+    limit--;
+  }
+
+  return fibonacci;
+}
+
+fibonacci(5);
+
+module.exports = fibonacci;
