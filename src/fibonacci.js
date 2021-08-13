d734d78874131d276b572b031604124b5c88acf8
Victor Benavente <templario0141@gmail.com>
2021-08-13 12:21:40 -0500

    Challenge 2 Fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,11 +1,24 @@
-/** 
+/**
  * Given a number, write a function that returns the "n" numbers of the fibonacci sequence
- *  
+ *
  * @param {number} limit - Number of fibonacci numbers to be generated
- * 
+ *
  * @returns {number[]}  - Fibonacci Serie
-*/
+ */
 
-function fibonacci(limit) { }
+function fibonacci(limit) {
+  let sum = 0;
+  let fibArray = [0, 1];
+  let index = 1;
+  while (true) {
+    if (fibArray.length == limit) {
+      return fibArray;
+    } else {
+      sum = fibArray[index] + fibArray[index - 1];
+      index += 1;
+      fibArray.push(sum);
+    }
+  }
+}
 
-module.exports = fibonacci
+module.exports = fibonacci;
