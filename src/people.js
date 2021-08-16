54a31fb59b853102b22884d6924a9b5032eb0efa
Kevin Cotrina <kcotrinam92@gmail.com>
2021-08-16 11:50:56 -0500

    Add implementation for peopleInformation method

--- src/people.js
+++ src/people.js
@@ -15,17 +15,37 @@
  * @property {Person} tallerPerson - Taller person information
  */
 
-/** 
+/**
  * The height in cm of several people was taken, in addition, the age of each person and extra information are also known.
- * An algorithm is required to calculate average age and average height. In addition, we need to know the information of the 
+ * An algorithm is required to calculate average age and average height. In addition, we need to know the information of the
  * tallest person, and the information of the youngest person.
- * 
+ *
  * @param {Person[]} people - People information to be processed
- * 
+ *
  * @returns {PeopleResponse}  - Processed information
-*/
+ */
 
+function peopleInformation(people) {
+  const heightProm = Math.floor(
+    people.reduce((acc, person) => acc + person.height, 0) / people.length,
+  );
+  const ageProm = Math.floor(
+    people.reduce((acc, person) => acc + person.age, 0) / people.length,
+  );
 
-function peopleInformation(people) { }
+  const tallerPerson = people.filter(
+    (person) => person.height > people[0].height,
+  )[0];
+  const youngerPerson = people.filter(
+    (person) => person.age < people[0].age,
+  )[0];
 
-module.exports = peopleInformation
+  return {
+    tallerPerson: tallerPerson,
+    youngerPerson: youngerPerson,
+    ageProm,
+    heightProm,
+  };
+}
+
+module.exports = peopleInformation;
