43b5eab3fd21bc26783facc835ee15faa9656b6d
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-10-26 21:16:18 -0500

    feat: Implement `fibonacci` function.

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,6 +6,21 @@
  * @returns {number[]}  - Fibonacci Serie
 */
 
-function fibonacci(limit) { }
+function fibonacci(limit) {
+    const numbers = []
+
+    let a = 0
+    let b = 1
+    let t = 1
+
+    while (numbers.length != limit) {
+        numbers.push(a)
+        t = a + b
+        a = b
+        b = t
+    }
+
+    return numbers
+}
 
 module.exports = fibonacci;
