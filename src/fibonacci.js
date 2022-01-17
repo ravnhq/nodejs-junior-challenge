cda7232129bcd74996b30ed61746460624274bd5
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-01-16 22:10:16 -0500

    feat: second exercise solved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,6 +6,37 @@
  * @returns {number[]}  - Fibonacci Serie
 */
 
-function fibonacci(limit) { }
+function fibonacci(limit) {
+
+  //Check if limit is greater than 0
+  if (limit > 0) {
+    //fibonacci numbers
+    let fibonacciNumbers = [0, 1];
+    let numberSequence = 0;
+    let previousNumber1 = 1;
+    let previousNumber2 = 0;
+
+    let index = 1;
+    let change = 0;
+
+    //Find fibonacci numbers
+    while (change == 0) {
+      index++;
+
+      numberSequence = previousNumber1 + previousNumber2;
+      previousNumber2 = previousNumber1;
+      previousNumber1 = numberSequence;
+      fibonacciNumbers.push(numberSequence);
+
+      if (index == limit - 1) {
+        change = 1;
+      }
+    }
+
+    return fibonacciNumbers;
+  }
+
+  return "The number of limit must be greater than 0";
+}
 
 module.exports = fibonacci;
