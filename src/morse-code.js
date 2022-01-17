828c611bee0f2e4d91578426ed5b3612d6daaa4c
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-01-17 00:32:33 -0500

    feat: third exercise solved

--- src/morse-code.js
+++ src/morse-code.js
@@ -8,6 +8,90 @@
  * @returns {string}  - String converted to english or morse code
 */
 
-function morseCode(message, convertTo) { }
+function morseCode(message, convertTo) {
+  const morse = {
+    A: '.-',
+    B: '-...',
+    C: '-.-.',
+    D: '-..',
+    E: '.',
+    F: '..-.',
+    G: '--.',
+    H: '....',
+    I: '..',
+    J: '.---',
+    K: '-.-',
+    L: '.-..',
+    M: '--',
+    N: '-.',
+    O: '---',
+    P: '.--.',
+    Q: '--.-',
+    R: '.-.',
+    S: '...',
+    T: '-',
+    U: '..-',
+    V: '...-',
+    W: '.--',
+    X: '-..-',
+    Y: '-.--',
+    Z: '--..',
+    0: '-----',
+    1: '.----',
+    2: '..---',
+    3: '...--',
+    4: '....-',
+    5: '.....',
+    6: '-....',
+    7: '--...',
+    8: '---..',
+    9: '----.',
+    ' ': ' ',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..',
+    '!': '-.-.--',
+    '@': '.--.-.',
+    '+': '.-.-.',
+    '-': '-....-',
+    '/': '-..-.',
+  };
+
+  function toMorse(phrase) {
+    const newMessage = phrase.toUpperCase();
+    let result = '';
+
+    for (let i = 0; i < newMessage.length; i += 1) {
+      result += `${morse[newMessage[i]]} `;
+    }
+
+    return result.trim();
+  }
+
+  function toEnglish(phrase) {
+    let result = '';
+
+    phrase
+      .split('   ')
+      .map((word) => word.split(' '))
+      .forEach((codeWord) => {
+        codeWord.forEach((codeChar) => {
+          result += Object.keys(morse).find((key) => morse[key] === codeChar);
+        });
+        result += ' ';
+      });
+
+    return result.trim();
+  }
+
+  if (convertTo === 'morse') {
+    return toMorse(message);
+  }
+  if (convertTo === 'english') {
+    return toEnglish(message);
+  }
+
+  return "Unrecognized option";
+}
 
 module.exports = morseCode;
