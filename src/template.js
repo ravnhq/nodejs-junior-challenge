6485e6d1709705d80d5858dc78936f6aa389fafb
jackelinepaola <jackeline@inka-labs.com>
2022-01-17 13:01:22 -0500

    fix: fixing the template challenge function to not use all the above logic and replace it with replace

--- src/template.js
+++ src/template.js
@@ -20,12 +20,7 @@
 */
 
 function template(model, character, message) {
-  let modelWithoutCharacters = model.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, " ").split(" ");
-  let modelWithoutGaps = modelWithoutCharacters.join("");
-  let stringRegex = "";
-  let processedMessage = "";
-  let newRegex = "";
-  let formattedString = "";
+  let formattedString = model;
 
   if (message.length === 0) {
     formattedString = "Invalid parameters";
@@ -34,30 +29,15 @@ function template(model, character, message) {
     formattedString = "Invalid parameters";
   }
   else {
-    for (let k = 0; k < modelWithoutGaps.length; k++) {
-      if (modelWithoutGaps[k] === character) {
-        if (message[k]) {
-          processedMessage += message[k];
-        }
-        else {
-          processedMessage += modelWithoutGaps[k];
-        }
-      } else {
-        processedMessage += modelWithoutGaps[k];
+    for (let i = 0; i < model.length; i++) {
+      if (message[i]) {
+        formattedString = formattedString.replace(character, message[i])
+      }else {
+         formattedString = formattedString.replace(character, character)
       }
     }
-
-    for (let i = 0; i < modelWithoutCharacters.length; i++) {
-      if (modelWithoutCharacters[i].length > 0) {
-        stringRegex += `([a-zA-Z0-9]{${modelWithoutCharacters[i].length}})`;
-      }
-
-    }
-
-    newRegex = new RegExp(stringRegex);
-    formattedString = processedMessage.replace(newRegex, "($1)$2-$3-$4");
-  }
-
+  };
+  
   return formattedString;
 }
 
