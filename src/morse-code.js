d76e2ab1178145fbd0d264a4eba4bf53594d1ef0
bllerenac <bllerenac@uni.pe>
2022-01-24 00:40:36 -0500

    add solution with tranform in object in morse code

--- src/morse-code.js
+++ src/morse-code.js
@@ -8,6 +8,49 @@
  * @returns {string}  - String converted to english or morse code
 */
 
-function morseCode(message, convertTo) { }
+// Object.entries(obj).forEach(([key, value]) => {
+      //   console.log(key + ' ' + value); // "a 5", "b 7", "c 9"
+      // });
+
+function morseCode(message, convertTo) {
+
+  let resultMessage = []
+
+  const MorseLetters = { 'A': '.-',   'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
+                         'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
+                         'O': '---',  'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-',
+                         'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----',
+                         '2': '..---','3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+                         '9': '----.', ' ': ' ', '.': '.-.-.-', ',': '--..--', '?': '..--..', '!': '-.-.--',
+                         '@': '.--.-.', '+': '.-.-.', '-': '-....-', '/': '-..-.',
+                        };
+  const messageLetters = convertTo === 'morse' ? message.toUpperCase().split(''):message.split('   ').map(word => ( word.split(' ')))
+  
+  switch (convertTo) {
+    case 'morse':
+      resultMessage = messageLetters.map( letter => {
+          return MorseLetters[letter]
+         })
+      break;
+  
+    default:
+      messageLetters.forEach(word => {
+          let wordEnglish = word.map(letter => {
+                              let wordLetters = ''
+                              Object.entries(MorseLetters).forEach(([key, value]) => {
+                                if( letter === value){
+                                  wordLetters = key
+                                }
+                              });
+                              return wordLetters
+                            })
+          resultMessage.push(wordEnglish.join(''))
+      })
+      break;
+  }
+
+  return resultMessage.join(' ')
+
+ }
 
 module.exports = morseCode;
