9317289586052289101cbc287ced34caee70d86c
cheshire <coren114@gmail.com>
2022-01-24 21:13:09 -0500

    improved people js

--- src/people.js
+++ src/people.js
@@ -27,17 +27,37 @@
 */
 
 function peopleInformation(people) { 
-  const listAge = people.map((person) => person.age);
-  const listHeight = people.map((person) => person.height);
-  const ageProm = listAge.reduce((a, b) => a + b) / people.length;
-  const heightProm = listHeight.reduce((a, b) => a + b) / people.length;
-  const younger = listAge.indexOf(Math.min(...listAge));
-  const taller = listHeight.indexOf(Math.max(...listHeight));
+  // const listAge = people.map((person) => person.age);
+  // const listHeight = people.map((person) => person.height);
+  // const ageProm = listAge.reduce((a, b) => a + b) / people.length;
+  // const heightProm = listHeight.reduce((a, b) => a + b) / people.length;
+  // const younger = listAge.indexOf(Math.min(...listAge));
+  // const taller = listHeight.indexOf(Math.max(...listHeight));
+  // return {
+  //   ageProm: Math.round(ageProm),
+  //   heightProm: Math.round(heightProm),
+  //   youngerPerson: people[younger],
+  //   tallerPerson: people[taller],
+  // };
+  if (!people.length) return null;
+  let counter = 0;
+  const age = [people[0].age, 0];
+  const height = [people[0].height, 0];
+  const total = people.reduce((personOne, personTwo) => {
+    if (age[0] > personTwo.age) { age[1] = counter + 1; }
+    if (height[0] < personTwo.height) { height[1] = counter + 1; }
+    counter += 1;
+    return {
+      age: personOne.age + personTwo.age,
+      height: personOne.height + personTwo.height,
+      
+    };
+  });
   return {
-    ageProm: Math.round(ageProm),
-    heightProm: Math.round(heightProm),
-    youngerPerson: people[younger],
-    tallerPerson: people[taller],
+    ageProm: total.age / (counter + 1),
+    heightProm: total.height / (counter + 1),
+    youngerPerson: people[age[1]],
+    tallerPerson: people[height[1]],
   };
 }
 
