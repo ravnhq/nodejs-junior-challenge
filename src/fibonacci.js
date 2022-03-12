9e748b70342dfb074e6727831626e4264b2f93f8
Michelle <michelle.pineda.rivas@gmail.com>
2022-03-12 01:22:40 -0600

    adding fibonacci challenge solution - tests passed

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,6 +6,20 @@
  * @returns {number[]}  - Fibonacci Serie
 */
 
-function fibonacci(limit) { }
+function fibonacci(limit) {
+    const Fibonacci = [];
+    let initial = 0;
+    let final = 1;
+    let sum = 0;
+
+    for (let i = 0; i < limit; i++) {
+        Fibonacci.push(initial)
+        sum = initial + final;
+        initial = final;
+        final = sum;
+    }
+
+    return Fibonacci;
+}
 
 module.exports = fibonacci;
