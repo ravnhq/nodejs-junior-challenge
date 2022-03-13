bb4e5b89bc670b0108b74bca9340041a9b1487a2
Michelle <michelle.pineda.rivas@gmail.com>
2022-03-12 18:35:28 -0600

    added morsecode solution - tests passed

--- src/morse-code.js
+++ src/morse-code.js
@@ -8,6 +8,76 @@
  * @returns {string}  - String converted to english or morse code
 */
 
-function morseCode(message, convertTo) { }
+const morseCodeAlphabet = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    ",": "--..--",
+    "?": "..--..",
+    "*": " ",
+    " ": "*",
+    "@": ".--.-.",
+    "/": "-..-.",
+    "+": ".-.-.",
+    "-": "-....-",
+    "0": '-----',
+    "1": '.----', 
+    "2": '..---', 
+    "3": '...--',
+    "4": '....-', 
+    "5": '.....', 
+    "6": '-....', 
+    "7": '--...', 
+    "8": '---..', 
+    "9": '----.'
+}
+
+function morseCode(message, convertTo) {
+    let convertedMessage = '';
+    let messageAsArray = message.toUpperCase()
+                                .replaceAll(' ', '*')
+                                .split('');
+    if (convertTo === 'morse') {
+        for (const letter of messageAsArray) {
+            convertedMessage += morseCodeAlphabet[letter] + ' ';
+        }
+    }
+    const objectKeys = Object.keys(morseCodeAlphabet);
+    const objectValues = Object.values(morseCodeAlphabet);
+    const messageAsArray2 = message.replaceAll('   ', ' * ')
+                                    .split(' ');
+    for (const element of messageAsArray2) { 
+        for (const i in objectValues) {
+            if (element === objectValues[i]) {
+                convertedMessage += objectKeys[i];
+            }
+        }
+    }
+
+    return convertedMessage.trim();
+}
 
 module.exports = morseCode;
