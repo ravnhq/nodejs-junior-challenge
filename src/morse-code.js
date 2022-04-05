84e3d61c83c6268a18513e334007f049a565ad84
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-04-05 11:25:20 -0600

    refactor: add refactor todo

--- src/morse-code.js
+++ src/morse-code.js
@@ -1,5 +1,3 @@
-const utilityFunctions = require('./utils');
-
 /** 
  * Write a program that automatically converts english text to morse code and vice versa, 
  * consider only use upper case letters when converting
@@ -10,12 +8,87 @@ const utilityFunctions = require('./utils');
  * @returns {string}  - String converted to english or morse code
 */
 
+// TODO: I used a placeholder value which is not oficially morse code. must refactor!
+const morseCodeAlphabet = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    ",": "--..--",
+    "?": "..--..",
+    "*": " ",
+    " ": "*",
+    "@": ".--.-.",
+    "/": "-..-.",
+    "+": ".-.-.",
+    "-": "-....-",
+    "0": '-----',
+    "1": '.----', 
+    "2": '..---', 
+    "3": '...--',
+    "4": '....-', 
+    "5": '.....', 
+    "6": '-....', 
+    "7": '--...', 
+    "8": '---..', 
+    "9": '----.'
+}
+
+const convertToMorse = message => {
+    let convertedMessage = '';
+    let messageAsArray = message.toUpperCase()
+                                .replaceAll(' ', '*')
+                                .split('');
+    for (const letter of messageAsArray) {
+        convertedMessage += morseCodeAlphabet[letter] + ' ';
+    }
+    return convertedMessage.trim();
+}
+
+const convertToEnglish = message => {
+    let convertedMessage = '';
+    const objectKeys = Object.keys(morseCodeAlphabet);
+    const objectValues = Object.values(morseCodeAlphabet);
+    const messageAsArray2 = message.replaceAll('   ', ' * ')
+                                   .split(' ');
+    for (const element of messageAsArray2) { 
+        for (const i in objectValues) {
+            if (element === objectValues[i]) {
+                convertedMessage += objectKeys[i];
+            }
+        }
+    }
+    return convertedMessage;
+}
+
 function morseCode(message, convertTo) {
     
     if (convertTo === 'morse') {
-        return utilityFunctions.convertToMorse(message);
+        return convertToMorse(message);
     } else {
-        return utilityFunctions.convertToEnglish(message)
+        return convertToEnglish(message)
     }
     
 }
