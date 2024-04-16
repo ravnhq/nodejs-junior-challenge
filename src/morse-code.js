c0cbee2b5a9cdcdeabf0e77ef58a88a8dd035708
Cristhian Ocola <ocolapintoc@gmail.com>
2024-04-16 08:19:59 -0500

    Feature:add_morse_solution

--- src/morse-code.js
+++ src/morse-code.js
@@ -8,6 +8,62 @@
  * @returns {string}  - String converted to english or morse code
 */
 
-function morseCode(message, convertTo) { }
+const morse = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    ",": "--..--",
+    "?": "..--..",
+    "*": "",
+    " ": " ",
+    "@": ".--.-.",
+    "/": "-..-.",
+    "+": ".-.-.",
+    "-": "-....-",
+    "0": '-----',
+    "1": '.----', 
+    "2": '..---', 
+    "3": '...--',
+    "4": '....-', 
+    "5": '.....', 
+    "6": '-....', 
+    "7": '--...', 
+    "8": '---..', 
+    "9": '----.'
+}
+const english = Object.fromEntries(Object.entries(morse).map(([letter, code]) => [code, letter]));
+
+function morseCode(message, convertTo) {
+
+    if (convertTo === 'morse') {
+        return message.split('').map(letter => morse[letter.toUpperCase()] || '').join(' ');
+    } else if (convertTo === 'english') {
+        return message.split(' ').map(code => english[code] || '').join('').replaceAll('**', ' ');
+    }
+    
+ }
 
 module.exports = morseCode;
