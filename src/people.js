f79b2099646460bd9a6069dc7be2ada0f0d7183c
Cristhian Ocola <ocolapintoc@gmail.com>
2024-04-16 09:16:26 -0500

    Refact:people_solution_only_one_reduce

--- src/people.js
+++ src/people.js
@@ -27,35 +27,74 @@
 */
 
 
-function calculateAgeProm(people) {
-    const totalAge = people.reduce((sum, person) => sum + person.age, 0);
-    return Math.round(totalAge / people.length);
-}
+// function calculateAgeProm(people) {
+//     const totalAge = people.reduce((sum, person) => sum + person.age, 0);
+//     return Math.round(totalAge / people.length);
+// }
 
-function calculateHeightProm(people) {
-    const totalHeight = people.reduce((sum,person) => sum + person.height, 0);
-    return Math.round(totalHeight / people.length);
-}
+// function calculateHeightProm(people) {
+//     const totalHeight = people.reduce((sum,person) => sum + person.height, 0);
+//     return Math.round(totalHeight / people.length);
+// }
 
-function findYoungerPerson(people) {
-    return people.reduce((youngest, person) => person.age < youngest.age ? person : youngest, people[0]);
-}
+// function findYoungerPerson(people) {
+//     return people.reduce((youngest, person) => person.age < youngest.age ? person : youngest, people[0]);
+// }
 
-function findTallerPerson(people) {
-    return people.reduce((tallest, person) => person.height > tallest.height ? person : tallest, people[0]);
-}
+// function findTallerPerson(people) {
+//     return people.reduce((tallest, person) => person.height > tallest.height ? person : tallest, people[0]);
+// }
+
+// function peopleInformation(people) { 
+
+//     PeopleResponse = {
+//         "ageProm" : calculateAgeProm(people),
+//         "heightProm" : calculateHeightProm(people),
+//         "youngerPerson" : findYoungerPerson(people),
+//         "tallerPerson" : findTallerPerson(people)
+//     }
+
+//     return PeopleResponse;
+    
+// }
 
 function peopleInformation(people) { 
 
-    PeopleResponse = {
-        "ageProm" : calculateAgeProm(people),
-        "heightProm" : calculateHeightProm(people),
-        "youngerPerson" : findYoungerPerson(people),
-        "tallerPerson" : findTallerPerson(people)
-    }
+    const init = {
+        totalAge: 0,
+        totalHeight: 0,
+        youngerPerson: people[0],
+        tallerPerson: people[0]
+    };
 
+    const metrics = people.reduce((acc, person) => {
+
+        //Total Edades
+        acc.totalAge += person.age;
+
+        //Total Alturas
+        acc.totalHeight += person.height;
+
+        //Calculo persona mas joven
+        if (person.age < acc.youngerPerson.age) {
+            acc.youngerPerson = person;
+        }
+
+        //Calculo persona mas alta
+        if (person.height > acc.tallerPerson.height) {
+            acc.tallerPerson = person;
+        }
+
+        return acc;
+    }, init);
+
+    const PeopleResponse = {
+        "ageProm" : Math.round(metrics.totalAge / people.length),
+        "heightProm" : Math.round(metrics.totalHeight / people.length),
+        "youngerPerson" : metrics.youngerPerson,
+        "tallerPerson" : metrics.tallerPerson,
+    }
     return PeopleResponse;
-    
 }
 
 module.exports = peopleInformation;
